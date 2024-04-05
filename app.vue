60e6387221ba14c5e831dc355252a470744f2045
Robin Klöckner <mail@robinkloeckner.com>
2024-04-05 17:20:39 +0200

    Initial commit.

---
+++ app.vue
@@ -0,0 +1,24 @@
+<script setup>
+const md = `
+# Welcome to docs4.dev
+
+Paragraphs can contain inline code \`const codeInline: string = 'highlighted code inline'\`{lang="ts"} with language prop.
+
+Code block:
+\`\`\` typescript  [index.js] {1, 3 - 5} additional meta data
+import {parseMarkdown} from '@nuxtjs/mdc/runtime'
+
+async function main(mdc: string) {
+  const ast = await parseMarkdown(mdc)
+
+  return ast
+}
+\`\`\`
+`
+</script>
+
+<template>
+  <div>
+    <MDC :value="md" />
+  </div>
+</template>
---
+++ components/prose/ProsePre.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="pre-wrapper">
+    <div v-if="filename">
+      {{ filename }}
+    </div>
+    <div v-if="language">
+      {{ language }}
+    </div>
+    <div v-if="highlights">
+      {{ highlights }}
+    </div>
+    <div v-if="meta">
+      {{ meta }}
+    </div>
+    <pre :class="$props.class"><slot/></pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  code: {
+    type: String,
+    default: ''
+  },
+  language: {
+    type: String,
+    default: null
+  },
+  filename: {
+    type: String,
+    default: null
+  },
+  highlights: {
+    type: Array as () => number[],
+    default: () => []
+  },
+  meta: {
+    type: String,
+    default: null
+  },
+  class: {
+    type: String,
+    default: null
+  }
+})
+</script>
+
+<style>
+.pre-wrapper > * {
+  margin-top: 0;
+  border: 1px solid #303030;
+}
+pre code .line {
+  display: block;
+}
+</style>
