b031ed43bf4ca9a6dbda785811f76a2829a00e0f
Robin Klöckner <mail@robinkloeckner.com>
2024-05-04 15:40:09 +0200

    Moved the ProsePre.vue component to the main components/ direcotry and registered it with the .global suffix instead.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,10 @@
 export default defineNuxtConfig({
   modules: ['@nuxtjs/mdc'],
   mdc: {
-    components: {
-      prose: true
-    },
     highlight: {
       theme: 'github-dark',
       langs: ['ts'],
       wrapperStyle: true
     }
-  },
-  components: {
-    global: true,
-    path: './components/prose'
   }
 })
